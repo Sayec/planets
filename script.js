a09157a67c615929a68e396f434fde823e99d4fb
Sayec <kamilbrzezowski@wp.pl>
2023-01-15 20:39:18 +0100

    transistion fixes, information section refactor, removed unused code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import data from './data.js';
 
 const menuButtons = document.querySelectorAll('nav ul li a');
-const planets = document.querySelectorAll('section');
 const container = document.querySelector('.container');
+const section = document.querySelector('section');
 const buttons = document.querySelectorAll('section .button');
 const planetsInformation = data();
 const sectionText = document.querySelector('section .text-container p');
@@ -24,34 +24,36 @@ buttons.forEach((button) => {
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].classList.remove('active');
     }
-
+    sectionText.style.opacity = 0;
     e.target.classList.add('active');
-    switch (typeOfInformation) {
-      case 'overview':
-        sectionText.innerHTML = planetsInformation[index].overview;
-        break;
-      case 'structure':
-        sectionText.innerHTML = planetsInformation[index].internalStructure;
-        break;
-    }
+    setTimeout(() => {
+      switch (typeOfInformation) {
+        case 'overview':
+          sectionText.innerHTML = planetsInformation[index].overview;
+          break;
+        case 'structure':
+          sectionText.innerHTML = planetsInformation[index].internalStructure;
+          break;
+      }
+      sectionText.style.opacity = 1;
+    }, 500);
   });
 });
 menuButtons.forEach((button) => {
   button.addEventListener('click', function (e) {
     e.preventDefault();
-
+    if (section.dataset.planetName === button.dataset.planetName) return;
     index = planetsInformation
       .map((e) => e.name)
       .indexOf(button.dataset.planetName);
-
-    container.style.transform = 'translate(-3%,0)';
+    container.style.transform = 'translate(-1%,0)';
     container.style.opacity = 0;
     setTimeout(() => {
       const sectionTitle = document.querySelector('section .text-container h1');
 
       const sectionImg = document.querySelector('section .photo-container img');
       const information = document.querySelectorAll('section .information p');
-
+      section.dataset.planetName = planetsInformation[index].name;
       sectionTitle.innerHTML = planetsInformation[index].namePL;
       switch (typeOfInformation) {
         case 'overview':
@@ -73,10 +75,10 @@ menuButtons.forEach((button) => {
 
       nav.classList.remove('visible');
       toggleMenu.classList.remove('close');
-    }, 1000);
+    }, 750);
     setTimeout(() => {
       container.style.transform = 'translate(0,0)';
       container.style.opacity = 1;
-    }, 1500);
+    }, 750);
   });
 });
